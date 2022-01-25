f17f6bcc49a4fe8e7333b25af045fc0688b380d5
adam teodoro <AdamTeodoro>
2022-01-24 21:31:34 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { rastrearEncomendas } from "correios-brasil/dist";
+
+async function rastrearEncomenda() {
+    await rastrearEncomendas(['QI583411291BR'])
+    .then((response) => {
+        if (response) {
+            const object = response[0];
+            const keys = Object.keys(object);
+            object[keys.length - 1].status
+        }
+    });
+}
+
+rastrearEncomenda()
