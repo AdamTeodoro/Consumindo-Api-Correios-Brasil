6eea3299a24508e5b9bdc92f1542a4b9bcde66e6
Adam Teodoro <66885644+AdamTeodoro@users.noreply.github.com>
2024-10-17 01:10:58 -0300

    Update index.ts
    alterando minha indentação

--- index.ts
+++ index.ts
@@ -2,13 +2,13 @@ import { rastrearEncomendas } from "correios-brasil/dist";
 
 async function rastrearEncomenda() {
     await rastrearEncomendas(['QI583411291BR'])
-    .then((response) => {
-        if (response) {
-            const object = response[0];
-            const keys = Object.keys(object);
-            object[keys.length - 1].status
-        }
-    });
+        .then((response) => {
+            if (response) {
+                const object = response[0];
+                const keys = Object.keys(object);
+                object[keys.length - 1].status
+            }
+        });
 }
 
 rastrearEncomenda()
